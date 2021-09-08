487a23bcd9ae08b817831e14cffad3c7902f5fe8
mukulverma2408 <mukulverma2408@gmail.com>
2021-09-08 23:14:10 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# This is my first programme in pycharm
+import matplotlib.pyplot as plt
+year = [1900, 1910, 1920, 1930, 1940, 1950]
+pop = [1.2, 2.4, 4.8, 8.1, 9.2,9.7]
+plt.plot(year, pop)
+plt.show()
