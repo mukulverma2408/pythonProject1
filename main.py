7d337f787feb29b52ff63d1845d51c6fd4d7ae98
mukulverma2408 <mukulverma2408@gmail.com>
2021-09-08 23:20:17 +0530

    Data Updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # This is my first programme in pycharm
 import matplotlib.pyplot as plt
-year = [1900, 1910, 1920, 1930, 1940, 1950]
-pop = [1.2, 2.4, 4.8, 8.1, 9.2,9.7]
+year = [1900, 1910, 1920, 1930, 1940, 1950,1960]
+pop = [1.2, 2.4, 4.8, 8.1, 9.2,9.7,10.31]
 plt.plot(year, pop)
 plt.show()
