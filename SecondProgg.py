fc0ced926a3597f3998ace011538182913219241
mukulverma2408 <mukulverma2408@gmail.com>
2023-08-16 08:36:48 +0530

    Age Calculator first itetration

--- SecondProgg.py
+++ SecondProgg.py
@@ -1,2 +1,9 @@
-Name = "Mukul"
-print(Name)
+import pandas as pd
+left = pd.read_csv("CAvideos.csv")
+right = pd.read_csv("GBvideos.csv")
+
+print(left.shape, right.shape)
+#merged_data= pd.merge(left, right, on='video_id')
+
+merged_data= pd.merge(left, right [['video_id', 'likes', 'views']], on='video_id', how='left' )
+print(merged_data.head)
